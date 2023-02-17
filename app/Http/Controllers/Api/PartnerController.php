546a7905872089cd4e2fef882fc58366efb382da
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2023-02-17 00:33:51 -0300

    initial partner bits

---
+++ app/Http/Controllers/Api/PartnerController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Partner;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+
+/*
+ * Managing partners is not important here, so the focus is showing all partners
+ * and mainly relaying orders data to them
+ * */
+class PartnerController extends Controller
+{
+    /**
+     * Display all partners
+     */
+    public function index() {
+        $partners = Partner::all();
+        return \response()->json([
+            'partners' => $partners
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): Response
+    {
+        //
+    }
+
+    /**
+     * Store a new ly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //
+    }
+
+    /**
+     * Display the specified partner.
+     */
+    public function show(Partner $partner): Response
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Partner $partner): Response
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Partner $partner): RedirectResponse
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Partner $partner): RedirectResponse
+    {
+        //
+    }
+}
