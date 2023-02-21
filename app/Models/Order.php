1bf47fb80eb5f2bcfa7e9ef6e9bebb99326935ac
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2023-02-21 13:35:50 -0300

    update relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Order extends Model
 {
@@ -28,5 +29,10 @@ class Order extends Model
     public function order_items(): HasMany
     {
         return $this->hasMany(OrderItem::class);
+    }
+
+    public function items(): HasManyThrough
+    {
+        return $this->hasManyThrough(Item::class, OrderItem::class, 'order_id', 'id');
     }
 }
--- database/factories/PartnerFactory.php
+++ database/factories/PartnerFactory.php
@@ -17,7 +17,7 @@ class PartnerFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name,
+            'name' => fake()->company,
             'description' => fake()->text(250),
             'uri' => fake()->url,
             'exchange_type' => fake()->randomElement(['API', 'CSV', 'FILE'])
